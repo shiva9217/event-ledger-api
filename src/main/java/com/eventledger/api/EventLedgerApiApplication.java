281175af70fd4b31febf9a5e13a71fc862163f10
shiva9217 <shivakrishnanagamalla17@gmail.com>
2026-05-23 08:41:10 +0530

    chore: initialize Spring Boot project with Maven and core dependencies

---
+++ src/main/java/com/eventledger/api/EventLedgerApiApplication.java
@@ -0,0 +1,12 @@
+package com.eventledger.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLedgerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventLedgerApiApplication.class, args);
+    }
+}
